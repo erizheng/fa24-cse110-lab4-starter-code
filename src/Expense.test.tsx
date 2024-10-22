4b248e39ae7f113f87a12c8e78eab3c130e2910a
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-10-21 17:17:56 -0700

    create test done
    Co-Authored-By: nlormsby <130116816+nlormsby@users.noreply.github.com>

--- src/Expense.test.tsx
+++ src/Expense.test.tsx
@@ -1,9 +1,86 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from './App';
 
-// describe("Expense Create", () => {
+describe("Expense Create", () => {
+    
+    test('Created Three Item in List', () => {
+        render(<App />);
 
-// }),
+        //get buttons and fields
+        const saveButton = screen.getByRole('button');
+        const nameBox = screen.getByTestId('ExpenseN');
+        const costBox = screen.getByTestId('ExpenseC');
+
+        //adds 3 expenses to the expenses list
+        fireEvent.change(nameBox, { target: { value: "Expense 1" } });
+        fireEvent.change(costBox, { target: { value: 50 } });
+        fireEvent.click(saveButton);
+        fireEvent.change(nameBox, { target: { value: "Expense 2" } });
+        fireEvent.change(costBox, { target: { value: 500 } });
+        fireEvent.click(saveButton);
+        fireEvent.change(nameBox, { target: { value: "Expense 3" } });
+        fireEvent.change(costBox, { target: { value: 33 } });
+        fireEvent.click(saveButton);
+        const xButton2 = screen.getByTestId('Expense 2');
+
+        //The three expense should be on screen
+        const exp1 = screen.getByText('Expense 1');
+        const exp2 = screen.getByText('Expense 2');
+        const exp3 = screen.getByText('Expense 3');
+
+        const exp1C = screen.getByText('$50');
+        const exp2C = screen.getByText('$500');
+        const exp3C = screen.getByText('$33');
+
+        expect(exp1).toBeInTheDocument,
+        expect(exp2).toBeInTheDocument,
+        expect(exp3).toBeInTheDocument,
+        expect(exp1C).toBeInTheDocument,
+        expect(exp2C).toBeInTheDocument,
+        expect(exp3C).toBeInTheDocument;
+
+    });
+
+    test('Total Spent and Remaining Are Updated', () => {
+        render(<App />);
+
+        
+        //get buttons and fields
+        const saveButton = screen.getByRole('button');
+        const nameBox = screen.getByTestId('ExpenseN');
+        const costBox = screen.getByTestId('ExpenseC');
+
+        //adds 3 expenses to the expenses list
+        fireEvent.change(nameBox, { target: { value: "Expense 1" } });
+        fireEvent.change(costBox, { target: { value: 50 } });
+        fireEvent.click(saveButton);
+        fireEvent.change(nameBox, { target: { value: "Expense 2" } });
+        fireEvent.change(costBox, { target: { value: 500 } });
+        fireEvent.click(saveButton);
+        fireEvent.change(nameBox, { target: { value: "Expense 3" } });
+        fireEvent.change(costBox, { target: { value: 33 } });
+        fireEvent.click(saveButton);
+        const xButton2 = screen.getByTestId('Expense 2');
+
+        //The three expense should be on screen
+        const exp1 = screen.getByText('Expense 1');
+        const exp2 = screen.getByText('Expense 2');
+        const exp3 = screen.getByText('Expense 3');
+
+        const spent = screen.getByTestId('Spending');
+        const remain = screen.getByTestId('Remainder');
+
+        expect(exp1).toBeInTheDocument,
+        expect(exp2).toBeInTheDocument,
+        expect(exp3).toBeInTheDocument,
+
+        //the screen should have 583 on it as the total spent so far
+        expect(spent.innerHTML).toContain("Spent so far: $583"),
+
+        //remaining should be 1000-583
+        expect(remain.innerHTML).toContain("Remaining: $417");
+    });
+}),
 describe("Expense Delete", () => {
     test('Successfully Removed', () => {
         render(<App />);
@@ -68,17 +145,6 @@ describe("Expense Delete", () => {
 
         const spent = screen.getByTestId('Spending');
         const remain = screen.getByTestId('Remainder');
-
-        expect(exp1).toBeInTheDocument,
-        expect(exp2).toBeInTheDocument,
-        expect(exp3).toBeInTheDocument,
-
-        //the screen should have 583 on it as the total spent so far
-        expect(spent.innerHTML).toContain("Spent so far: $583"),
-
-        //remaining should be 1000-583
-        expect(remain.innerHTML).toContain("Remaining: $417"),
-
         //delete exp2
         fireEvent.click(xButton2),
 
