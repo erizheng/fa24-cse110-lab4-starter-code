33bf0b0c81d0fa7ef40d7c9bfbb1e81052785cfb
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-11-04 15:36:10 -0800

    setup done

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,39 +1,61 @@
 import { Expense } from "../types";
 import { Request, Response } from "express";
+import { Database } from "sqlite";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
-    const { id, cost, description } = req.body;
 
-    if (!description || !id || !cost) {
-        return res.status(400).send({ error: "Missing required fields" });
-    }
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
 
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
+ 
+        if (!description || !id || !cost) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+        res.status(201).send({ id, description, cost });
+ 
+    } catch (error) {
+ 
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
+ 
+ }
 
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
-}
+// export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
+//     const { id, cost, description } = req.body;
 
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
-    // TO DO: Implement deleteExpense function
-    const { id, cost, description } = req.body;
+//     if (!description || !id || !cost) {
+//         return res.status(400).send({ error: "Missing required fields" });
+//     }
 
-    const curExpense: Expense = {
-        id: id,
-        description,
-        cost,
-    };
+//     const newExpense: Expense = {
+//         id: id,
+//         description,
+//         cost,
+//     };
 
-    const index = expenses.findIndex((expense) => expense.id == id);
+//     expenses.push(newExpense);
+//     res.status(201).send(newExpense);
+// }
 
-    expenses.splice(index);
-    res.status(200).send(expenses);
-}
+// export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+//     // TO DO: Implement deleteExpense function
+//     const { id, cost, description } = req.body;
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
-}
+//     const curExpense: Expense = {
+//         id: id,
+//         description,
+//         cost,
+//     };
+
+//     const index = expenses.findIndex((expense) => expense.id == id);
+
+//     expenses.splice(index);
+//     res.status(200).send(expenses);
+// }
+
+// export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
+//     res.status(200).send({ "data": expenses });
+// }
