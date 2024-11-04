13e54467dac11ec9ce7b68a2412a25aff13d81ab
nlormsby <nlormsby@gmail.com>
2024-11-04 13:41:01 -0800

    try pushing budget useeffect

--- client/src/components/Budget/Budget.tsx
+++ client/src/components/Budget/Budget.tsx
@@ -1,7 +1,26 @@
 import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
+import { fetchBudget } from "../../utils/budget-utils";
+
+
 const Budget = () => {
-  const { expenses, setExpenses, budget} = useContext(AppContext);
+  const { budget, setBudget } = useContext(AppContext);
+  
+  useEffect(() => {
+    loadBudget();
+    }, []);
+  
+    // Function to load expenses and handle errors
+    const loadBudget = async () => {
+    try {
+      const budgetB = await fetchBudget();
+      setBudget(budgetB);
+    } catch (err: any) {
+      console.log(err.message);
+    }
+    };
+  
+
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
       <div>Budget: ${budget}</div>
